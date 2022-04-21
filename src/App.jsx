f5bd5046cee18665dea0ece2ca65d7a42bc752b7
Joe Kirschnik <kirschnik@wisc.edu>
2022-04-20 20:14:32 -0500

    STuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import './App.css';
 
+function getNumWorkDays(startDate, endDate) {
+  var numWorkDays = 0;
+  var currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+      // Skips Sunday and Saturday
+      if (currentDate.getDay() !== 0 && currentDate.getDay() !== 6) {
+          numWorkDays++;
+      }
+      currentDate = currentDate.addDays(1);
+  }
+  return numWorkDays;
+}
+
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
 function App() {
   const currentTime = new Date();
-  const lastDay = new Date(2022,7,12)
+  const lastDay = new Date(2022,7,12);
   const diffTime = Math.abs(lastDay - currentTime);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const daysRemaining = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const workDays = getNumWorkDays(currentTime, lastDay);
   return (
     <>
     <header>
     Cora Countdown to last day of work (8/12)
     </header>
       <body className="App-header">
-        <h1>{diffDays + ' days'}</h1>
+        <h1>{daysRemaining + ' days'}</h1>
+        <h1>{workDays + ' work days'}</h1>
       </body>
       </>
   );
